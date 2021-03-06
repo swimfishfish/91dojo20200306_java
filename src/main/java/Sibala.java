604e4ac9acd054787b2e6a9b3f75c44a81a7b9d2
mengchihchiang <mengchihchiang@ami.com.tw>
2021-03-06 14:56:06 +0800

    Zen

--- src/main/java/Sibala.java
+++ src/main/java/Sibala.java
@@ -5,11 +5,28 @@ public class Sibala {
         Player player1 = new Player(s[0]);
         Player player2 = new Player(s[1]);
 
-        compare(player1, player2);
+        Player won = compare(player1, player2);
+
+        if (won != null) {
+            return won.name + " wins. all the same kind:" + won.dices.get(0);
+        }
         return "Tie";
     }
 
     private Player compare(Player player1, Player player2) {
 
+
+        int pt1 = player1.dices.get(0);
+        int pt2 = player2.dices.get(0);
+
+        Category cate = getCategory(player1.dices);
+
+        if (pt1 < pt2) {
+            return player2;
+
+        }
+        else {
+            return player1;
+        }
     }
 }
